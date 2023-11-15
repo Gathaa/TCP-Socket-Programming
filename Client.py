4cc02416f6bfac4ce90bf63d4532272647006778
Aimen Guedhami <114817360+Gathaa@users.noreply.github.com>
2023-11-15 22:07:25 +0100

    Quiz Working Fix Some UI +Add Timer as an interface

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import tkinter as tk
 
 # Initialize the client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,16 +9,16 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('192.168.1.2', 3000)
 client_socket.connect(server_address)
 
-def receive_messages(sock):
-    """ Receives messages from the server and prints them. """
+def receive_messages(sock, text_widget):
+    """ Receives messages from the server and updates the UI. """
     while True:
         try:
-            message = sock.recv(1024).decode()
+            message = sock.recv(1024).decode().lower()
             if not message:
                 print("Disconnected from server.")
                 sock.close()
                 break
-            print(message)
+            text_widget.insert(tk.END, message + "\n")
         except OSError:  # Possibly client has left the game.
             break
         except Exception as e:
@@ -25,24 +26,38 @@ def receive_messages(sock):
             sock.close()
             break
 
+def send_answer(entry, sock):
+    """ Sends the answer to the server. """
+    answer = entry.get()
+    sock.send(answer.lower().encode())
+    entry.delete(0, tk.END)  # Clear the entry field
+
 def main():
     # Send username to the server
     username = input("Enter your username:")
     client_socket.send(username.encode())
 
+    # Create the UI window
+    root = tk.Tk()
+    root.title("Quiz Game")
+
+    # Create a text widget to display messages
+    text_widget = tk.Text(root, height=10, width=40)
+    text_widget.pack()
+
+    # Create an entry widget for the answer
+    entry = tk.Entry(root, width=60)
+    entry.pack()
+
+    # Create a button to submit the answer
+    submit_button = tk.Button(root, text="Submit", command=lambda: send_answer(entry, client_socket))
+    submit_button.pack()
+
     # Start the thread to receive messages from the server
-    threading.Thread(target=receive_messages, args=(client_socket,)).start()
+    threading.Thread(target=receive_messages, args=(client_socket, text_widget)).start()
 
-    # Continuously read answers and send them to the server
-    try:
-        while True:
-            answer = input()
-            print()
-            client_socket.send(answer.encode())
-    except Exception as e:
-        print(f"Error sending message: {e}")
-    finally:
-        client_socket.close()
+    # Start the Tkinter main loop
+    root.mainloop()
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
--- Server.py
+++ Server.py
@@ -4,37 +4,38 @@ import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('192.168.1.2', 3000))
-server_socket.listen(2)  # Listen for 2 clients
+server_socket.listen(2)
 
 print("Server initialized. Waiting for clients to connect...")
 
 clients = []
 
-questions = [
-    {"question": "What is the capital of France?", "options": ["Paris", "Berlin", "London", "Madrid"], "answer": "Paris"},
-    {"question": "Which planet is known as the Red Planet?", "options": ["Mars", "Venus", "Jupiter", "Mercury"], "answer": "Mars"},
-    {"question": "What is the largest mammal in the world?", "options": ["Elephant", "Blue Whale", "Giraffe", "Lion"], "answer": "Blue Whale"},
-    {"question": "What element does 'O' represent on the periodic table?", "options": ["Osmium", "Oxygen", "Gold", "Carbon"], "answer": "Oxygen"},
-    {"question": "Who painted the Mona Lisa?", "options": ["Vincent van Gogh", "Pablo Picasso", "Claude Monet", "Leonardo da Vinci"], "answer": "Leonardo da Vinci"}
+questions_and_options = [
+    ["What is the capital of France?", ["Paris", "Berlin", "London", "Madrid"]],
+    ["Which planet is known as the Red Planet?", ["Mars", "Venus", "Jupiter", "Mercury"]],
+    ["What is the largest mammal in the world?", ["Elephant", "Blue Whale", "Giraffe", "Lion"]],
+    ["What element does 'O' represent on the periodic table?", ["Osmium", "Oxygen", "Gold", "Carbon"]],
+    ["Who painted the Mona Lisa?", ["Vincent van Gogh", "Pablo Picasso", "Claude Monet", "Leonardo da Vinci"]]
 ]
+
+answers = ["Paris", "Mars", "Blue Whale", "Oxygen", "Leonardo da Vinci"]
 
 
 def handle_client(client_socket, address, username):
     score = 0
-    for question_info in questions:
-        question_text = question_info["question"]
-        options = question_info["options"]
-        correct_answer = question_info["answer"].lower()       
-        # Send question and options to the specific client
+
+    
+    for (question_text, options), correct_answer in zip(questions_and_options, answers):
         send_to_all_clients(f"{username}, {question_text}")
         send_to_all_clients(f"Options: {', '.join(options)}")
         client_socket.settimeout(30.0)
         try:
-            client_answer = client_socket.recv(1024).decode().strip().lower()
-            print(f"Received answer from {username}: {client_answer}")
-            print(f"Correct answer: {correct_answer}")
+            full_message = client_socket.recv(1024).decode().strip().lower()
+            print(f"Received answer from {username}: {full_message}")
+            client_answer = full_message.split()[-1]
 
-            if client_answer == correct_answer or client_answer in options:
+            # Check if the client's answer is correct
+            if client_answer == correct_answer.lower():
                 send_to_all_clients(f"{username} answered correctly!")
                 score += 1
             else:
@@ -55,7 +56,7 @@ def send_to_all_clients(message):
         try:
             client.send(message.encode())
         except:
-            pass  # Ignore errors which are mostly due to closed connections
+            pass
 
 while True:
     client_socket, client_address = server_socket.accept()
@@ -65,8 +66,6 @@ while True:
     print(f"{username} joined the game!")
 
     clients.append((client_socket, client_address, username))
-
-    # Check if there is at least one client before starting the thread
     if len(clients) >= 1:
         send_to_all_clients("Game starting! Get ready for the quiz!")
         threading.Thread(target=handle_client, args=(clients[0][0], clients[0][1], clients[0][2])).start()
