1b3caa8474577a807e0696273506870bb8316227
Arij Fradi <arij.fradi@medtech.tn>
2023-11-16 12:27:02 +0100

    Integrated UI with the functionalities

--- Client.py
+++ Client.py
@@ -1,63 +1,163 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
 import socket
 import threading
-import tkinter as tk
+import json
 
-# Initialize the client socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class QuizClientUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Modern Quiz Client")
+        self.master.geometry("800x600")
+        self.master.config(bg="white")
 
-# Connect to the server
-server_address = ('192.168.1.2', 3000)
-client_socket.connect(server_address)
+        # Initialize socket
+        self.client_socket = None
 
-def receive_messages(sock, text_widget):
-    """ Receives messages from the server and updates the UI. """
-    while True:
+        # UI setup
+        self.setup_ui()
+
+        # Thread for listening to server messages
+        self.listen_thread = None
+
+    def setup_ui(self):
+        style = ttk.Style()
+        style.configure("TButton", padding=10, font=("Helvetica", 14), background="#9400D3", foreground="white", borderwidth=0, highlightthickness=0)
+
+        self.username_label = tk.Label(self.master, text="Enter your username:", font=("Helvetica", 16), bg="white", fg="black")
+        self.username_label.pack(pady=(300, 10))
+
+        self.username_entry = tk.Entry(self.master, font=("Helvetica", 14))
+        self.username_entry.pack(pady=(20, 10))
+
+        self.connect_button = tk.Button(self.master, text="     Connect     ", command=self.connect_to_server)
+        self.connect_button.configure(bg="#9400D3", fg="white", font=("Helvetica", 14), pady=10, bd=0)
+        self.connect_button.pack(pady=30)
+
+        self.connection_label = tk.Label(self.master, text="", font=("Helvetica", 16), bg="white", fg="#9400D3")
+        self.connection_label.pack(pady=40)
+
+        self.attempt_button = tk.Button(self.master, text="Attempt Quiz", command=self.attempt_quiz)
+        self.attempt_button.configure(bg="#9400D3", fg="white", font=("Helvetica", 14), pady=10, bd=0)
+
+        self.options_frame = tk.Frame(self.master, bg="white")
+        self.var_option = tk.StringVar()
+        style.configure("TRadiobutton", font=("Helvetica", 14), background="white", foreground="black")
+
+        self.option_radio_a = ttk.Radiobutton(self.options_frame, text="", variable=self.var_option, value="A", style="TRadiobutton", padding=(10, 5))
+        self.option_radio_b = ttk.Radiobutton(self.options_frame, text="", variable=self.var_option, value="B", style="TRadiobutton", padding=(10, 5))
+        self.option_radio_c = ttk.Radiobutton(self.options_frame, text="", variable=self.var_option, value="C", style="TRadiobutton", padding=(10, 5))
+        self.option_radio_d = ttk.Radiobutton(self.options_frame, text="", variable=self.var_option, value="D", style="TRadiobutton", padding=(10, 5))
+
+        self.option_radio_a.grid(row=0, column=0, padx=40, pady=(10, 0))
+        self.option_radio_b.grid(row=0, column=1, padx=10, pady=(10, 0))
+        self.option_radio_c.grid(row=1, column=0, padx=10, pady=(10, 0))
+        self.option_radio_d.grid(row=1, column=1, padx=10, pady=(10, 0))
+
+        self.submit_button = tk.Button(self.master, text="Submit Answer", command=self.submit_answer)
+        self.submit_button.configure(bg="#9400D3", fg="white", font=("Helvetica", 14), pady=10, bd=0)
+
+        self.message_label = tk.Label(self.master, text="", font=("Helvetica", 14), bg="white", fg="#9400D3")
+
+        self.question_label = tk.Label(self.master, text="Question:", font=("Helvetica", 18, "bold"), bg="white", fg="black")
+
+        self.answer_status_label = tk.Label(self.master, text="", font=("Helvetica", 14), bg="white", fg="green")
+
+        self.options_frame.pack_forget()
+        self.submit_button.pack_forget()
+        self.message_label.pack_forget()
+        self.attempt_button.pack_forget()
+        self.question_label.pack_forget()
+        self.answer_status_label.pack_forget()
+
+    def connect_to_server(self):
+        username = self.username_entry.get().strip()
+        if not username:
+            messagebox.showerror("Error", "Please enter a username.")
+            return
+
         try:
-            message = sock.recv(1024).decode().lower()
-            if not message:
-                print("Disconnected from server.")
-                sock.close()
-                break
-            text_widget.insert(tk.END, message + "\n")
-        except OSError:  # Possibly client has left the game.
-            break
-        except Exception as e:
-            print(f"Error: {e}")
-            sock.close()
-            break
+            server_address = ('192.168.51.65', 3000)
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect(server_address)
+            self.client_socket.send(username.encode())
 
-def send_answer(entry, sock):
-    """ Sends the answer to the server. """
-    answer = entry.get()
-    sock.send(answer.lower().encode())
-    entry.delete(0, tk.END)  # Clear the entry field
+            self.listen_thread = threading.Thread(target=self.listen_to_server, daemon=True)
+            self.listen_thread.start()
+
+            self.update_connection_message("Connected successfully!")
+            self.connect_button.config(state=tk.DISABLED)
+            self.username_entry.config(state=tk.DISABLED)
+            self.attempt_button.pack()
+
+        except Exception as e:
+            messagebox.showerror("Connection Error", f"Unable to connect to the server: {e}")
+            self.update_connection_message("Connection failure!")
+
+    def listen_to_server(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
+                if message:
+                    self.process_server_message(message)
+            except Exception as e:
+                print(f"Error: {e}")
+                break
+
+    def process_server_message(self, message):
+        if message.startswith("Question:"):
+            question_data = json.loads(message[len("Question:"):])
+            self.update_question_and_options(question_data)
+        elif "Correct!" in message or "Incorrect!" in message:
+            self.update_answer_status(message, "green" if "Correct!" in message else "red")
+            time.sleep(1)  # Small delay before requesting the next question
+            self.client_socket.send("request_question".encode())
+
+    def attempt_quiz(self):
+        self.username_label.pack_forget()
+        self.username_entry.pack_forget()
+        self.connect_button.pack_forget()
+        self.connection_label.pack_forget()
+        self.attempt_button.pack_forget()
+
+        self.question_label.pack()
+        self.options_frame.pack(pady=10)
+        self.submit_button.pack(pady=(30, 20))
+        self.message_label.pack()
+        self.answer_status_label.pack()
+
+        self.client_socket.send("request_question".encode())
+
+    def submit_answer(self):
+        answer = self.var_option.get().strip()
+        if not answer:
+            messagebox.showerror("Error", "Please select an answer.")
+            return
+        self.client_socket.send(json.dumps({"answer": answer}).encode())
+
+    def update_connection_message(self, message):
+        self.connection_label.config(text=message)
+
+    def update_question_and_options(self, question_info):
+        question = question_info["question"]
+        options = question_info["options"]
+
+        self.question_label.config(text=f"Question: {question}")
+        self.option_radio_a.config(text=f"A: {options[0]}")
+        
+        self.option_radio_b.config(text=f"B: {options[1]}")
+        self.option_radio_c.config(text=f"C: {options[2]}")
+        self.option_radio_d.config(text=f"D: {options[3]}")
+
+        self.var_option.set(None)  # Reset the selected option
+
+    def update_answer_status(self, status, color):
+        self.answer_status_label.config(text=status, fg=color)
 
 def main():
-    # Send username to the server
-    username = input("Enter your username:")
-    client_socket.send(username.encode())
-
-    # Create the UI window
     root = tk.Tk()
-    root.title("Quiz Game")
-
-    # Create a text widget to display messages
-    text_widget = tk.Text(root, height=10, width=40)
-    text_widget.pack()
-
-    # Create an entry widget for the answer
-    entry = tk.Entry(root, width=60)
-    entry.pack()
-
-    # Create a button to submit the answer
-    submit_button = tk.Button(root, text="Submit", command=lambda: send_answer(entry, client_socket))
-    submit_button.pack()
-
-    # Start the thread to receive messages from the server
-    threading.Thread(target=receive_messages, args=(client_socket, text_widget)).start()
-
-    # Start the Tkinter main loop
+    app = QuizClientUI(root)
     root.mainloop()
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
--- Server.py
+++ Server.py
@@ -1,15 +1,39 @@
 import socket
 import threading
+import json
 import time
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('192.168.1.2', 3000))
-server_socket.listen(2)
+def handle_client(client_socket, address, username):
+    score = 0
+    for question, options in questions_and_options:
+        question_data = json.dumps({"question": question, "options": options})
+        client_socket.send(f"Question:{question_data}".encode())
+        client_socket.settimeout(30.0)
+        
+        try:
+            answer = json.loads(client_socket.recv(1024).decode())
+            if answer["answer"].lower() == answers[questions_and_options.index([question, options])].lower():
+                score += 1
+                client_socket.send("Correct!".encode())
+            else:
+                client_socket.send(f"Incorrect! Correct answer: {answers[questions_and_options.index([question, options])]}".encode())
+            time.sleep(2)  # Small delay before next question
+        except socket.timeout:
+            client_socket.send(f"Time's up! The correct answer was {answers[questions_and_options.index([question, options])]}.".encode())
+        except Exception as e:
+            print(f"An error occurred with client {username}: {e}")
+            break
 
+    client_socket.send(f"Your final score is {score}".encode())
+    client_socket.close()
+    clients.remove((client_socket, address, username))
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('192.168.51.65', 3000))
+server_socket.listen(2)
 print("Server initialized. Waiting for clients to connect...")
 
 clients = []
-
 questions_and_options = [
     ["What is the capital of France?", ["Paris", "Berlin", "London", "Madrid"]],
     ["Which planet is known as the Red Planet?", ["Mars", "Venus", "Jupiter", "Mercury"]],
@@ -17,55 +41,12 @@ questions_and_options = [
     ["What element does 'O' represent on the periodic table?", ["Osmium", "Oxygen", "Gold", "Carbon"]],
     ["Who painted the Mona Lisa?", ["Vincent van Gogh", "Pablo Picasso", "Claude Monet", "Leonardo da Vinci"]]
 ]
-
 answers = ["Paris", "Mars", "Blue Whale", "Oxygen", "Leonardo da Vinci"]
-
-
-def handle_client(client_socket, address, username):
-    score = 0
-
-    
-    for (question_text, options), correct_answer in zip(questions_and_options, answers):
-        send_to_all_clients(f"{username}, {question_text}")
-        send_to_all_clients(f"Options: {', '.join(options)}")
-        client_socket.settimeout(30.0)
-        try:
-            full_message = client_socket.recv(1024).decode().strip().lower()
-            print(f"Received answer from {username}: {full_message}")
-            client_answer = full_message.split()[-1]
-
-            # Check if the client's answer is correct
-            if client_answer == correct_answer.lower():
-                send_to_all_clients(f"{username} answered correctly!")
-                score += 1
-            else:
-                send_to_all_clients(f"{username} answered incorrectly. Correct answer: {correct_answer}")
-        except socket.timeout:
-            send_to_all_clients(f"{username}, time's up! The correct answer was {correct_answer}.")
-        except Exception as e:
-            print(f"An error occurred with client {username}: {e}")
-            break
-        time.sleep(2)
-
-    client_socket.send(f"{username}, your final score is {score}".encode())
-    client_socket.close()
-    clients.remove((client_socket, address, username))
-
-def send_to_all_clients(message):
-    for client, _, _ in clients:
-        try:
-            client.send(message.encode())
-        except:
-            pass
 
 while True:
     client_socket, client_address = server_socket.accept()
     print(f"Accepted connection from {client_address}")
-
     username = client_socket.recv(1024).decode().strip()
     print(f"{username} joined the game!")
-
     clients.append((client_socket, client_address, username))
-    if len(clients) >= 1:
-        send_to_all_clients("Game starting! Get ready for the quiz!")
-        threading.Thread(target=handle_client, args=(clients[0][0], clients[0][1], clients[0][2])).start()
+    threading.Thread(target=handle_client, args=(client_socket, client_address, username)).start()
